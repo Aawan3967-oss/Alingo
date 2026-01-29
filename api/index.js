0d477aea427f783c7d19d67639a57c83c7fe30f4
Aawan3967-oss <asifawan3967@gmail.com>
2026-01-29 15:22:24 +0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -2,8 +2,11 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+// یہ لائن سب سے اہم ہے
 app.use(express.static(path.join(__dirname, '../public')));
 
-app.get('*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
+
+module.exports = app;
