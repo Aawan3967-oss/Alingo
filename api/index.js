91e47bc76d0ac40f0759d15084f77c0b11986145
Aawan3967-oss <asifawan3967@gmail.com>
2026-01-29 15:16:21 +0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -7,10 +7,3 @@ app.use(express.static(path.join(__dirname, '../public')));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
-
-// جب کوئی ایڈمن پیج کھولے
-app.get("/admin", (req, res) => {
-  res.sendFile(path.join(__dirname, "../public/admin.html"));
-});
-
-module.exports = app;
