f2ca07bcb98b0934d9baeb1559ded5214f00ca3e
Aawan3967-oss <asifawan3967@gmail.com>
2026-01-29 21:25:10 +0500

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,23 +1,8 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
 const app = express();
 
-// بتاتا ہے کہ تمام فائلیں public فولڈر میں ہیں
-app.use(express.static(path.join(__dirname, '../public')));
-
-// مین ہوم پیج
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-});
-
-// ٹیکسی بکنگ پیج کا راستہ (یہ ایرر ختم کرے گا)
-app.get('/taxi', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/taxi.html'));
-});
-
-// ایڈمن پینل کا راستہ
-app.get('/admin', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/admin.html'));
+app.get("/api/health", (req, res) => {
+  res.json({ status: "Alingo API Running" });
 });
 
 module.exports = app;
