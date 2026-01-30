c700fbd6006b9caece5eb44ce8d48fb67b1ec800
Aawan3967-oss <asifawan3967@gmail.com>
2026-01-30 09:12:03 +0500

    Delete app directory

--- app/dashboard.js
+++
@@ -1,9 +0,0 @@
-fetch("sidebar.html")
-  .then(res => res.text())
-  .then(html => {
-    document.getElementById("sidebar").innerHTML = html;
-  });
-
-function loadModule(file) {
-  document.getElementById("moduleFrame").src = `modules/${file}`;
-}
