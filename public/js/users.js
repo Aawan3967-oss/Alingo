cc12e606f000f0c1b604442683867e0a58e3f7fb
Aawan3967-oss <asifawan3967@gmail.com>
2026-01-31 10:04:57 +0500

    Create users.js

---
+++ public/js/users.js
@@ -0,0 +1,8 @@
+function blockUser(id){
+  document.getElementById("status"+id).innerText = "Blocked";
+  alert("User Blocked");
+}
+
+function resetPoints(id){
+  alert("Points reset after admin review");
+}
