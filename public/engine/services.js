f68703eb28af221d4a8661fa6bd1216320955f5f
Aawan3967-oss <asifawan3967@gmail.com>
2026-01-31 14:37:58 +0500

    Create services.js

---
+++ public/engine/services.js
@@ -0,0 +1,39 @@
+export const SERVICES = {
+  food: {
+    platform: 2.5,
+    seller: 80,
+    rider: 15,
+    investor: 2,
+    tax: 0.5
+  },
+
+  ride: {
+    platform: 2.5,
+    driver: 85,
+    investor: 10,
+    tax: 2.5
+  },
+
+  shopping: {
+    platform: 2.5,
+    seller: 82,
+    courier: 12,
+    investor: 3,
+    tax: 0.5
+  },
+
+  logistics: {
+    platform: 2.5,
+    distributor: 75,
+    courier: 15,
+    investor: 6,
+    tax: 1.5
+  },
+
+  tickets: {
+    platform: 2.5,
+    vendor: 90,
+    investor: 5,
+    tax: 2.5
+  }
+};
